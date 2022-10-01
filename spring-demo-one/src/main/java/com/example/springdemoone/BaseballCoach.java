34bd671537a20cb43f87db06d7fd8c914d451db5
Charu Kush <charu.kush@gmail.com>
2022-10-01 13:06:23 -0700

    https://github.com/charukush/spring-demo.git

---
+++ spring-demo-one/src/main/java/com/example/springdemoone/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.example.springdemoone;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Do 30 mins of workout";
+	}
+}
---
+++ spring-demo-one/src/main/java/com/example/springdemoone/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.example.springdemoone;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Run a hard 5k";
+	}
+
+}
